0c0e9d28ae7f8347ea0a89b27d463330e3da459b
katjacrusius <89479212+katjacrusius@users.noreply.github.com>
2025-05-10 16:46:21 -0700

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 
 const Navbar = ({ showChatbot, setShowChatbot }) => (
-  <header className="fixed top-0 left-0 w-full h-16 bg-[#BAD7DF] flex justify-between items-center px-8 shadow-md z-50 text-[#2C3E50]">
+  <header className="fixed top-0 left-0 w-full h-16 bg-[#FFE5B4] flex justify-between items-center px-8 shadow-md z-50 text-[#5cb8f6]">
     {/* Logo */}
     <div className="text-2xl font-semibold">TrueNorth</div>
 
     {/* Navigation */}
     {!showChatbot && (
       <nav className="space-x-6 flex items-center">
-        <a href="#team" className="text-[#2C3E50] px-6 py-2 rounded-full hover:bg-[#99DDCC] transition">Team</a>
-        <a href="#mission" className="text-[#2C3E50] px-6 py-2 rounded-full hover:bg-[#99DDCC] transition">Mission</a>
-        <a href="#contact" className="text-[#2C3E50] px-6 py-2 rounded-full hover:bg-[#99DDCC] transition">Contact</a>
+        <a
+          href="#team"
+          className="text-[#5cb8f6] px-6 py-2 rounded-full hover:bg-[#f3ae99] hover:text-[#b74deb] transition"
+        >
+          Team
+        </a>
+        <a
+          href="#mission"
+          className="text-[#5cb8f6] px-6 py-2 rounded-full hover:bg-[#f3ae99] hover:text-[#b74deb] transition"
+        >
+          Mission
+        </a>
+        <a
+          href="#contact"
+          className="text-[#5cb8f6] px-6 py-2 rounded-full hover:bg-[#f3ae99] hover:text-[#b74deb] transition"
+        >
+          Contact
+        </a>
       </nav>
     )}
   </header>
 );
 
 export default Navbar;
-
